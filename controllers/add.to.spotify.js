069663bbe0996417f96ef0c442ae3e5adddb9dab
Rafael Bueno <rafaelbuenolink@gmail.com>
2019-09-17 22:43:40 -0300

    change to await

--- controllers/add.to.spotify.js
+++ controllers/add.to.spotify.js
@@ -5,27 +5,15 @@ const spotifyApi = new SpotifyWebApi({
   clientSecret: process.env.APPSECRET,
 });
 
-function addToSpotify(user, spotifyTracksIdArray) {
+async function addToSpotify(user, spotifyTracksIdArray) {
   spotifyApi.setAccessToken(user.access_token);
 
   const currentDate = new Date();
   const playlistName = `Sounder-app ${currentDate}`;
 
-  spotifyApi.createPlaylist(user.spotifyId, playlistName, { public: false })
-    .then((playlistData) => {
-      console.log('Created playlist! ', playlistData);
+  const addPlaylsit = await spotifyApi.createPlaylist(user.spotifyId, playlistName, { public: false });
+  const addTracks = await spotifyApi.addTracksToPlaylist(addPlaylsit.body.id, spotifyTracksIdArray);
 
-      // add track to this created playlist:
-      // spotifyApi.addTracksToPlaylist(playlistData.body.id, ['spotify:track:4iV5W9uYEdYUVa79Axb7Rh', 'spotify:track:1301WleyT98MSxVHPZCA6M'])
-      spotifyApi.addTracksToPlaylist(playlistData.body.id, spotifyTracksIdArray)
-        .then((tracksData) => {
-          console.log('Added tracks to playlist!', tracksData);
-        }, (err) => {
-          console.log('adding track went wrong!', err);
-        });
-    }, (err) => {
-      console.log('Something went wrong!', err);
-    });
 }
 
 module.exports = { addToSpotify };
--- routes/private/spotify-routes.js
+++ routes/private/spotify-routes.js
@@ -3,9 +3,9 @@ const { addToSpotify } = require('../../controllers/add.to.spotify');
 
 const spotifyRouter = express.Router();
 
-
 spotifyRouter.get('/add-to-spotify', (req, res) => {
-  const spotifyTracksIdArray = [] // aguardando função do pedro
+  // aguardando função spotifyTracksIdArray do Pedro
+  const spotifyTracksIdArray = ['spotify:track:4iV5W9uYEdYUVa79Axb7Rh', 'spotify:track:1301WleyT98MSxVHPZCA6M'];
   addToSpotify(req.user, spotifyTracksIdArray);
   res.render('private/discovery/index', req.user);
 });
