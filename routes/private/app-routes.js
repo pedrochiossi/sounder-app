9b38a2c728daa33b7b56bd847459a60bbe1b1237
Rafael Bueno <rafaelbuenolink@gmail.com>
2019-09-18 20:53:30 -0300

    refactor

--- routes/private/app-routes.js
+++ routes/private/app-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const trackController = require('../../controllers/track.controller');
+const { addToSpotify } = require('../../controllers/playlist.controller.js');
 
 router.get('/discovery', async (req, res) => {
   const { user } = req;
@@ -15,6 +16,13 @@ router.get('/discovery', async (req, res) => {
   } catch (err) {
     console.log(err);
   }
+});
+
+router.get('/add-to-spotify', (req, res) => {
+  // aguardando função spotifyTracksIdArray do Pedro
+  const spotifyTracksIdArray = ['spotify:track:4iV5W9uYEdYUVa79Axb7Rh', 'spotify:track:1301WleyT98MSxVHPZCA6M'];
+  addToSpotify(req.user, spotifyTracksIdArray);
+  res.render('private/discovery/index', req.user);
 });
 
 module.exports = router;
--- routes/public/index.js
+++ routes/public/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  if (req.user) {
+    res.redirect('/discovery');
+    return;
+  }
   res.render('public/index');
 });
 
