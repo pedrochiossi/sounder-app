0fbd609430278e16e88815bce6630d6d8d7ca091
Rafael Bueno <rafaelbuenolink@gmail.com>
2019-09-20 11:17:30 -0300

    save playlist to db

--- models/Playlist.js
+++ models/Playlist.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
+// const playlistSchema = new Schema({
+//   tracks: [{ type: Schema.Types.ObjectId, ref: 'Track' }],
+//   name: { type: String, required: true },
+//   user: { type: Schema.Types.ObjectId, ref: 'User' },
+//   created_at: Date,
+//   spotify_id: String,
+//   images: [String],
+// });
+
 const playlistSchema = new Schema({
   tracks: [{ type: Schema.Types.ObjectId, ref: 'Track' }],
   name: { type: String, required: true },
@@ -10,7 +19,6 @@ const playlistSchema = new Schema({
   spotify_id: String,
   images: [String],
 });
-
 const Playlist = mongoose.model('Playlist', playlistSchema);
 
 module.exports = Playlist;
