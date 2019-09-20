232954c6342e4f373b3c5b5fe29167a50d1ad887
Rafael Bueno <rafaelbuenolink@gmail.com>
2019-09-20 11:23:56 -0300

    clean code

--- models/Playlist.js
+++ models/Playlist.js
@@ -2,15 +2,6 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-// const playlistSchema = new Schema({
-//   tracks: [{ type: Schema.Types.ObjectId, ref: 'Track' }],
-//   name: { type: String, required: true },
-//   user: { type: Schema.Types.ObjectId, ref: 'User' },
-//   created_at: Date,
-//   spotify_id: String,
-//   images: [String],
-// });
-
 const playlistSchema = new Schema({
   tracks: [{ type: Schema.Types.ObjectId, ref: 'Track' }],
   name: { type: String, required: true },
