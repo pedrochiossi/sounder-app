528436f0b3b1dbb188955d3f8bc6908fafaac807
pedrochiossi <pchiossi@gmail.com>
2021-02-02 09:49:51 -0300

    refactor - fetch track from logged user on discovery page

--- client/src/pages/Discovery/index.tsx
+++ client/src/pages/Discovery/index.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback, useContext } from 'react';
+import api from '../../services/api';
+import { UserContext } from '../../context/user';
+import './discovery.scss'
 
-const Discovery: React.FC = () => {
+interface Track {
+  name: string;
+  spotify_id: string;
+  preview_url: string;
+  artists: object[];
+  album: object;
+};
+
+const Discovery: React.FC  = () => {
+
+  const [track, setTrack] = useState<Track>();
+  const { user } = useContext(UserContext);
+
+  const fetchTrack = useCallback(async () => {
+    const response = await api.get('/tracks/new');
+    if (response.status === 200) {
+      const { track } = response.data;
+      setTrack(track);
+    }
+  },[])
+
+  useEffect(() => {
+    fetchTrack();
+  },[user, fetchTrack])
+
   return (
-    <div style={{color: 'white'}}>
-      Discovery Page
+    <div className="track-container">
+      <span>{track && track.name}</span>
     </div>
   )
 }
