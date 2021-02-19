a272ee8a3dd193e25a76f7c3b0c32d9fbdaf6573
pedrochiossi <pchiossi@gmail.com>
2021-02-19 02:09:42 -0300

    fix - change redirect logic on spotify login, remove cookie on logout

--- routes/public/auth.routes.js
+++ routes/public/auth.routes.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
+const url = require('url');
 const router = express.Router();
 const userController = require('../../controllers/user.controller');
 
@@ -22,17 +23,29 @@ router.get(
 
 router.get('/auth/spotify/callback',
   passport.authenticate('spotify', { failureRedirect: `${process.env.CLIENT_URL}/` }), (req, res) => {
-    res.redirect(`${process.env.CLIENT_URL}/discovery`);
+    const { access_token } = req.user;
+    res.redirect(url.format({
+      pathname: `${process.env.CLIENT_URL}/`,
+      query: {
+        redirected: true
+      }
+    }))
 });
 
 router.get('/logout', (req, res) => {
   req.logOut();
+  req.session.destroy();
+  res.clearCookie('connect.sid', { path: '/' });
   res.status(200).json({success: true, message: 'Logout successfully'})
 });
 
-router.post('/auth/refresh', async (req, res) => {
-  await userController.resetToken(req.user);
-  return res.status(200).json({ success: true, message: 'Token refreshed successfully' });
+router.post('/auth/refresh', async (req, res, next) => {
+  try {
+    await userController.resetToken(req.user);
+    return res.status(200).json({ success: true, message: 'Token refreshed successfully' });
+  } catch(error) {
+    next(error);
+  }
 });
 
 
